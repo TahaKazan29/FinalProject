57cc5f9518ca251e122eaa00953ca1c1ff73726d
TahaKazan29 <tahakazantk@gmail.com>
2021-02-06 23:57:36 +0300

    6 Şubat 2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,18 +18,6 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public void Add(Product product)
-        {
-            if (product.ProductName.Length >= 4)
-            {
-                _productDal.Add(product);
-            }
-            else
-            {
-                Console.WriteLine("Ürün Adı Çok Kısa");
-            }
-        }
-
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -42,6 +31,11 @@ namespace Business.Concrete
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
