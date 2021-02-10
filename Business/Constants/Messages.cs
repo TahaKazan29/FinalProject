9bcd764df80504f708ec2e7151c29b8e407d8303
TahaKazan29 <tahakazantk@gmail.com>
2021-02-11 00:10:55 +0300

    10 Şubat 2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "Ürünler Listelendi";
+    }
+}
